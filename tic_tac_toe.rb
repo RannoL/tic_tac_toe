f833a602a99704b7f1ffa6ed2a4680545e9d3106
RannoLauri <rannolauri@gmail.com>
2020-06-02 13:29:44 +0300

    Switching between two players and checks if coordinate is occupied or not, main game loop, no winner or draw check yet

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,20 +1,18 @@
 # Makes,renders and changes the board
 class Board
+  attr_accessor :rows
   def initialize
     @walls = ['||', '|', '|', '|| ']
     @cell_index = 1
     @row_value = ''
-    @rows = {
-    header:"_____________________",
-    row1:"",
-    row2:"",
-    row3:"",
-    footer:"---------------------"
-    }
+    @rows = { header:"_____________________",
+              row1:"",
+              row2:"",
+              row3:"",
+              footer:"---------------------" }
     make_board
     render_board
   end
-
 
   def make_board
     for row in 1...4
@@ -35,6 +33,17 @@ class Board
     @rows.each_value do |row|
       puts row
       puts '---------------------'
+    end
+  end
+
+  def find_n_replace_coord(coord,piece)
+
+    for row in 1...4
+      if rows["row#{row}".to_sym].include?(coord)
+        row_content = rows["row#{row}".to_sym]
+        row_content[coord] = piece.to_s
+        rows["row#{row}".to_sym] = row_content
+      end
     end
   end
 end
@@ -69,18 +78,32 @@ class Game
   end
 
   def two_players_play
-    @player_one = Player.new("Player 1", :X, @board)
-    @player_two = Player.new("Player 2", :O, @board)
-    @player_one.ask_for_coordinate
+    @player_one = Player.new('Player 1', :X, @board)
+    @player_two = Player.new('Player 2', :O, @board)
+    @current_player = @player_one
+    loop do
+      @board.render_board
+      @current_player.get_coordinate
+      switch_players
+    end
   end
 
   # Play against computer
   def one_player_play
-    @player_one = Player.new("Player 1", :X, @board)
+    @player_one = Player.new('Player 1', :X, @board)
+  end
+
+  def switch_players
+    if @current_player == @player_one
+      @current_player = @player_two
+    else
+      @current_player = @player_one
+    end
   end
 end
   
 class Player
+  @@made_moves = []
   attr_accessor :name, :piece
 
   def initialize(name, piece,board)
@@ -90,10 +113,40 @@ class Player
     puts "#{@name} is #{@piece}"
   end
 
+  # Ask for users input
   def ask_for_coordinate
     puts '---------------------'
     puts "#{@name}'s move: "
     @move = gets.chomp
+  end
+
+  # Verify and add to the board
+  def get_coordinate
+    loop do
+      coord = ask_for_coordinate
+      if verify_coord(coord)
+        @board.find_n_replace_coord(coord, @piece)
+        @@made_moves << coord
+        break
+      end
+    end
+  end
+
+  def verify_coord(coord)
+    begin
+      coord = Integer(coord)
+
+      if @@made_moves.include?(coord)
+        puts 'Already occupied!'
+      elsif coord.between?(1, 9)
+        true
+        @@made_moves << coord
+      else
+        puts 'Choose a coordinate from the board between 1 and 9'
+      end
+    rescue ArgumentError, TypeError
+      puts 'Choose a coordinate from the board between 1 and 9'
+    end
   end
 end
 
